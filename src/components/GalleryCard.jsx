40b0b00df126588c779badd7946a045b97a54208
yosuke20 <tangi.tdp2023.A@gmail.com>
2024-08-23 08:49:11 +0900

    カードの配列を追加

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -2,6 +2,47 @@ import React from "react";
 import "../scss/GalleryCard.scss";
 import GamingImg from "../images/gaming-img.png";
 
+// カードで使用するデータの配列
+const cardData = [
+  {
+    imgSrc: "../images/gaming-img.png",
+    title: "GamingAirways",
+    text1: "Branding",
+    text2: "coding/design",
+  },
+  {
+    imgSrc: "../images/card-img1.jpg",
+    title: "AdventureQuest",
+    text1: "Marketing",
+    text2: "strategy/design",
+  },
+  {
+    imgSrc: "../images/card-img2.jpg",
+    title: "AdventureQuest",
+    text1: "Marketing",
+    text2: "strategy/design",
+  },
+  {
+    imgSrc: "../images/card-img3.jpg",
+    title: "AdventureQuest",
+    text1: "Marketing",
+    text2: "strategy/design",
+  },
+  {
+    imgSrc: "../images/card-img4.jpg",
+    title: "AdventureQuest",
+    text1: "Marketing",
+    text2: "strategy/design",
+  },
+  {
+    imgSrc: "../images/card-img5.jpg",
+    title: "AdventureQuest",
+    text1: "Marketing",
+    text2: "strategy/design",
+  },
+  // 追加のカードデータ
+];
+
 const GalleryCard = () => {
   return (
     <>
