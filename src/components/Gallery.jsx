2a339d1c2f8d38fb2b629eae4876c687ea75009b
yosuke20 <tangi.tdp2023.A@gmail.com>
2024-09-02 20:20:17 +0900

    homeへ戻るボタンを実装

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "../scss/Gallery.scss";
 import GalleryCard from "./GalleryCard";
-
+import { Link } from "react-router-dom";
 const Gallery = () => {
   return (
     <div className="Gallery">
+      <div className="back">
+        <Link to="/">Back</Link>
+      </div>
       <div className="gallery-card-wrapper">
         <GalleryCard />
       </div>
