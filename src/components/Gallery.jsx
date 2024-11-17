06fbe828185ec3f4e9506fe5205d51d40e14ad4e
yosuke20 <tangi.tdp2023.A@gmail.com>
2024-11-17 17:01:57 +0900

    backボタンとcardのmarginを調整

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 const Gallery = () => {
   return (
     <div className="Gallery">
-      <div className="back">
-        <Link to="/">Back</Link>
-      </div>
       <div className="gallery-card-wrapper">
         <GalleryCard />
+      </div>
+      <div className="back">
+        <Link to="/">Back</Link>
       </div>
     </div>
   );
