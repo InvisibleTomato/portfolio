bcf141511e7f9a61a849fa4775419c6a46e1f2ff
yosuke20 <tangi.tdp2023.A@gmail.com>
2024-11-27 00:05:09 +0900

    スクロール禁止の処理をHomeのみに変更

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../scss/Home.scss";
 import CustomCursor from "./CustomCursor";
 
 const Home = () => {
+  // スクロール禁止の設定
+  useEffect(() => {
+    // Homeページ表示時にbodyにクラスを追加
+    document.body.classList.add("no-scroll");
+
+    // クリーンアップ時にクラスを削除
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, []); // 空の依存配列でマウント・アンマウント時に実行
   return (
     <div>
       <CustomCursor />
