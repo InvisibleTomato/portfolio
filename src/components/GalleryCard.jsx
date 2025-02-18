d8816e3c995c9dcf99bf4d276481c97d1078f7b3
InvisibleTomato <tangi.tdp2023.a@gmail.com>
2025-02-18 14:03:41 +0900

    cardのurlを修正

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -9,21 +9,21 @@ import ColaImg from "../images/craft-cola.png";
 const cardData = [
   {
     imgSrc: GamingImg,
-    imgUrl: "https://invisibletomato23.github.io/GamingAirways/",
+    imgUrl: "https://invisibletomato.github.io/GamingAirways/",
     title: "GamingAirways",
     text1: "Branding / Solo Project",
     text2: "Design / Coding",
   },
   {
     imgSrc: SyowaImg,
-    imgUrl: "https://invisibletomato23.github.io/syowa/",
+    imgUrl: "https://invisibletomato.github.io/syowa/",
     title: "Syowaチョコレート",
     text1: "Branding / Team Project",
     text2: "Coding Only",
   },
   {
     imgSrc: ColaImg,
-    imgUrl: "https://invisibletomato23.github.io/tokyo-craft-cola/",
+    imgUrl: "https://invisibletomato.github.io/tokyo-craft-cola/",
     title: "TOKYOくらふとコーラ",
     text1: "Branding / Solo Project",
     text2: "Design / Coding",
